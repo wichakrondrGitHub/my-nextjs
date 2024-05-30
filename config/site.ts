4cea2da4cfab9da20633ee30257fafc3069b4be0
Dear <wichakrondr@gmail.com>
2024-05-31 00:23:27 +0700

    Update : meta data site

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,9 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Vending Machine",
+  description:
+    "this site create by nextjs+nextui , animation with framer motion ",
   navItems: [
     {
       label: "HOME",
